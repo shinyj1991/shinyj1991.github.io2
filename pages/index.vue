bef55a9029d32e4c342e4aabb337e0183ff2d796
Yongjun SHIN <63823237+shinyj1991@users.noreply.github.com>
2021-11-04 15:56:44 +0900

    ,

--- pages/index.vue
+++ pages/index.vue
@@ -10,5 +10,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#home {padding: 50px 50px 100px;}
+#home {padding: 50px 50px 100px;
+  blockquote {background: #1e1e1e; padding: 1em; font-family: 'Nanum Myeongjo'; font-size: 1em;
+    p {margin: 0; padding: 0.5em 1em; border-left: 3px solid #ffffff;}
+  }
+}
 </style>
