774509c19d1d8d4678e74d49cca5cb0199ba658e
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-04-28 16:44:19 +0900

    blog list : fetch => asyncData

--- pages/blog/list/_category.vue
+++ pages/blog/list/_category.vue
@@ -9,24 +9,25 @@
 
 <script>
 export default {
-  data() {
+  async asyncData({ $content, params, store }) {
+    const path = params.category.replace(/_/gi, '/');
+    const visibleLength = 10;
+    const totalArticles = await $content(path, { deep: true }).only([]).fetch();
+    const lastPage = Math.ceil(totalArticles.length / visibleLength);
+
+    let articles = await this.$content(path, { deep: true })
+      .limit(visibleLength)
+      .sortBy('date', 'desc')
+      .fetch()
+
     return {
+      visibleLength,
+      lastPage,
+      articles,
       page: 1,
-      visibleLength: 0,
-      lastPage: 0,
-      category: null,
-      articles: []
-    }
-  },
-  computed: {
-    path: function() {
-      return this.$route.params.category ? this.$route.params.category.replace(/_/gi, '/') : '/'
     }
   },
   watch: {
-    path: async function() {
-      this.get_articles()
-    },
     page: async function(newVal, oldVal) {
       let articles = await this.$content(this.path, { deep: true })
         .sortBy('date', 'desc')
@@ -35,32 +36,6 @@ export default {
         .fetch()
 
       this.articles = [...this.articles, ...articles];
-    }
-  },
-  created() {
-    console.log('created', this.path)
-    this.get_articles()
-  },
-  methods: {
-    async get_articles() {
-      const path = this.$route.params.category ? this.$route.params.category.replace(/_/gi, '/') : '/';
-      const visibleLength = 10;
-      const totalArticles = await this.$content(path, { deep: true }).only([]).fetch();
-
-      // console.log(totalArticles.map(file => `/blog/${file.path.replace(/\//gi, '_').slice(1)}`))
-
-      let articles = await this.$content(path, { deep: true })
-        .limit(visibleLength)
-        .sortBy('date', 'desc')
-        .fetch()
-
-      this.visibleLength = visibleLength;
-      this.lastPage = Math.ceil(totalArticles.length / visibleLength);
-      this.articles = articles;
-    },
-    formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return new Date(date).toLocaleDateString('ko', options)
     }
   }
 }
