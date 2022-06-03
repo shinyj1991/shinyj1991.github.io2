c6ab15d250c9b2890c14be470569ed56faf56c4a
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-06-03 18:25:26 +0900

    viewport 제거

--- middleware/index.js
+++ middleware/index.js
@@ -3,6 +3,18 @@ export default function ({ store, route }) {
 
   // console.log('global middleware', route.name)
 
+  /*if (process.client) {
+    const screeWidth = window.screen.width;
+    const minWidth = 600;
+
+    if (screeWidth < minWidth) {
+      const ratio = minWidth / screeWidth;
+      document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=${ratio}, maximum-scale=${ratio}, minimum-scale=${ratio}, user-scalable=no`);
+    } else {
+      document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=1, user-scalable=yes`);
+    }
+  }*/
+
   if (route.name === 'article-list-category') {
     // list
     store.commit('set_blog_depth', route.params.category ? route.params.category.split('_') : []);
--- plugins/index.js
+++ plugins/index.js
@@ -1,8 +1,16 @@
 export default async (context) => {
   context.store.commit("app_loaded");
-  
-  if (process.client) {
-    const ratio = window.screen.width / window.innerWidth;
-    document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=${ratio}, maximum-scale=${ratio}, minimum-scale=${ratio}, user-scalable=yes`);
-  }
+
+  /*if (process.client) {
+    const screeWidth = window.screen.width;
+    const minWidth = 600;
+
+    if (screeWidth < minWidth) {
+      const ratio = screeWidth / minWidth;
+      document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=${ratio}, maximum-scale=${ratio}, minimum-scale=${ratio}, user-scalable=no`);
+    } else {
+      document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=1, user-scalable=yes`);
+    }
+  }*/
 };
+ 
