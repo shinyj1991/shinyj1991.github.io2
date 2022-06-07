81b35ff85058a9605679fcddb17ec1ba95f4d13c
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-06-07 09:51:41 +0900

    롤백

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,7 @@ export default {
     },
     meta: [
       { charset: "utf-8" },
-      // { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "viewport", content: "width=600, initial-scale=1" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [
