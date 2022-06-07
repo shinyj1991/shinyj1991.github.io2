625e5c87aed2b899c19fa20ea8dd17b05f7e1bed
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-06-08 06:53:42 +0900

    쿠팡 광고 추가

---
+++ components/banner/coupang.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="banner-coupang">
+    <iframe 
+      src="https://ads-partners.coupang.com/widgets.html?id=587213&template=carousel&trackingCode=AF7769340&subId=&width=900&height=140"
+      frameborder="0"
+      scrolling="no"
+      referrerpolicy="unsafe-url"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    script: [
+      {
+        hid: 'coupang',
+        src: 'https://ads-partners.coupang.com/g.js',
+        defer: true
+      }
+    ]
+  }),
+  data: () => ({
+    banner: null
+  }),
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.banner-coupang {
+  iframe {
+    width: 100%;
+  }
+}
+</style>
