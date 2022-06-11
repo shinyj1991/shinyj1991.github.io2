0f6c50e313856156082a71621fdfaf0efadf965f
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-06-11 10:10:25 +0900

    변진섭 - 새들처럼 : 제작

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,19 @@
-import { resolve } from "path";
-import getRoutes from "./utils/getRoutes";
+import { resolve } from 'path';
+import getRoutes from './utils/getRoutes';
 
 export default {
-  target: "static",
+  target: 'static',
   head: {
     htmlAttrs: {
-      lang: "ko",
+      lang: 'ko',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
   },
   loading: '@/components/loader.vue',
@@ -21,30 +21,32 @@ export default {
     port: 8000,
   },
   alias: {
-    images: resolve(__dirname, "./assets/images"),
+    images: resolve(__dirname, './assets/images'),
   },
-  css: ["@/assets/css/reset.scss"],
+  css: ['@/assets/css/reset.scss'],
   components: true,
   buildModules: [
-    "@nuxt/typescript-build",
+    '@nuxt/typescript-build',
     ['@nuxtjs/google-gtag', {
       id: 'G-7K43G97QC2',
       config: {
-        anonymize_ip: true, // anonymize IP 
-        send_page_view: false, // might be necessary to avoid duplicated page track on page reload
+        anonymize_ip: true,
+        send_page_view: false,
         linker: {
           domains: ['exit5.co.kr']
         }
       },
-      debug: true, // enable to track in dev mode
-      disableAutoPageTrack: false, // disable if you don't want to track each page route with router.afterEach(...).
+      debug: true,
+      disableAutoPageTrack: false,
     }],
   ],
   modules: [
-    ["nuxt-highlightjs", { style: "obsidian" }],
-    "@nuxt/content",
-    "@nuxtjs/sitemap",
-    "@nuxtjs/axios",
+    ['nuxt-highlightjs', {
+      style: 'obsidian'
+    }],
+    '@nuxt/content',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/axios',
   ],
   content: {
     dir: 'content/public'
@@ -53,15 +55,18 @@ export default {
     routes: () => getRoutes()
   },
   styleResources: {
-    scss: ["~/assets/css/_variable.scss", "~/assets/css/_mixin.scss"],
+    scss: [
+      '~/assets/css/_variable.scss',
+      '~/assets/css/_mixin.scss'
+    ],
   },
   build: {},
   sitemap: {
     hostname: 'https://exit5.co.kr/',
     routes: () => getRoutes()
   },
-  plugins: ["~/plugins/index.js"],
+  plugins: ['~/plugins/index.js'],
   router: {
-    middleware: ["index"],
+    middleware: ['index'],
   }
 };
